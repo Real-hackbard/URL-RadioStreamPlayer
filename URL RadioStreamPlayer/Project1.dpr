8782c11013f617e28c6ca4554275b0880af78a55
hackbard <96856062+Real-hackbard@users.noreply.github.com>
2026-07-01 07:23:13 +0200

    Add files via upload

---
+++ URL RadioStreamPlayer/Project1.dpr
@@ -0,0 +1,26 @@
+program Project1;
+
+uses
+  Forms,
+  BASSPlayer in 'BASSPlayer.pas',
+  Unit1 in 'Unit1.pas' {Form1},
+  DSPPluginCtrl in 'DSPPluginCtrl.pas' {DSPControlForm},
+  AddonConfig in 'AddonConfig.pas' {AddonConfigForm},
+  VisPluginCtrl in 'VisPluginCtrl.pas' {VisControlForm},
+  PluginConfig in 'PluginConfig.pas' {PluginConfigForm},
+  TAGEdit in 'TAGEdit.pas' {TAGEditForm},
+  MPEGInfoBox in 'MPEGInfoBox.pas' {MPEGFileInfoForm},
+  OGGInfoBox in 'OGGInfoBox.pas' {OggVorbisInfoForm},
+  WMAInfoBox in 'WMAInfoBox.pas' {WMAInfoForm};
+
+{$R *.res}
+
+begin
+  System.IsMultiThread := true;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDSPControlForm, DSPControlForm);
+  Application.CreateForm(TAddonConfigForm, AddonConfigForm);
+  Application.CreateForm(TVisControlForm, VisControlForm);
+  Application.Run;
+end.
